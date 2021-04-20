d6c3bda384e7cf015ec72cc3adbe095fe93aebf0
Vlad Shcherbachenia <vladshcherbachenia@gmail.com>
2021-04-20 14:37:23 +0300

    first commit

---
+++ src/components/game.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Board from "./board";
+import './game.css';
+import './other.css';
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
